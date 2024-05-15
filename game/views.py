32b2ab7df278cf96e84ed7785e97fec3856c0fc6
HidekiOkumura <okumurahidekikangeki@gmail.com>
2024-05-15 12:40:40 +0900

    rename symbol

--- game/views.py
+++ game/views.py
@@ -3,14 +3,14 @@ from game.models import TypingWord
 
 def problems(request):
     DEFAULT_NUM_RESPONSE_WORDS = 15
-    num_words_in_database = TypingWord.objects.all().count()
-    num_response_words = DEFAULT_NUM_RESPONSE_WORDS
+    num_words_in_db = TypingWord.objects.all().count()
+    num_words_for_response = DEFAULT_NUM_RESPONSE_WORDS
 
-    if num_words_in_database < num_response_words:
+    if num_words_in_db < num_words_for_response:
         response = JsonResponse({"words": [word.word for word in TypingWord.objects.all()]})
         return response
     
     # Chose random words from the database
-    words_to_response = TypingWord.objects.order_by('?')[:num_response_words]
-    response = JsonResponse({"words": [word.word for word in words_to_response]})
+    words_for_response = TypingWord.objects.order_by('?')[:num_words_for_response]
+    response = JsonResponse({"words": [word.word for word in words_for_response]})
     return response
